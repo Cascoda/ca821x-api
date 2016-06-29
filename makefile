f0b3d9600cbe81adaf253b7599e7ca99c3b58594
Ciaran Woodward <ciaran@cascoda.com>
2016-06-29 09:26:04 +0100

    Fixed race condition & added makefile
    Fixed the race condition in kernel_exchange.c which could break
    synchronous responses. Now a mutex is used to control read access to the
    driver.

---
+++ makefile
@@ -0,0 +1,26 @@
+TARGET = libcascoda.a
+LIBS = -lm
+CC = gcc
+CFLAGS = -g -Wall -pthread
+INCLUDEDIR = include/
+SOURCEDIR = source/
+
+.PHONY: default all clean
+
+default: $(TARGET)
+all: default
+
+OBJECTS = $(patsubst %.c, %.o, $(wildcard $(SOURCEDIR)*.c))
+HEADERS = $(wildcard $(INCLUDEDIR),*.h)
+
+%.o: %.c $(HEADERS)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+.PRECIOUS: $(TARGET) $(OBJECTS)
+
+$(TARGET): $(OBJECTS)
+	ar rcs $(TARGET) $^
+
+clean:
+	-rm -f $(SOURCEDIR)*.o
+	-rm -f $(TARGET)
--- source/kernel_exchange.c
+++ source/kernel_exchange.c
@@ -22,6 +22,7 @@ static int ca8210_test_int_exchange(
 
 static int DriverFileDescriptor;
 static pthread_t rx_thread;
+static pthread_mutex_t rx_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 /******************************************************************************/
 
@@ -31,7 +32,9 @@ static void *ca8210_test_int_read_worker(void *arg)
 	size_t rx_len;
 	/* TODO: while not told to exit? */
 	while (1) {
+		pthread_mutex_lock(&rx_mutex);
 		rx_len = read(DriverFileDescriptor, rx_buf, 0);
+		pthread_mutex_unlock(&rx_mutex);
 		if (rx_len > 0) {
 			cascoda_downstream_dispatch(rx_buf, rx_len);
 		}
@@ -72,13 +75,19 @@ static int ca8210_test_int_exchange(
 )
 {
 	int status, Rx_Length;
+	uint8_t isSynchronous = ((buf[0] & SPI_SYN) && response);
+
+	if(isSynchronous) pthread_mutex_lock(&rx_mutex);	//Enforce synchronous write then read
+
 	ca8210_test_int_write(buf, len);
 
-	if ((buf[0] & SPI_SYN) && response) {
+	if (isSynchronous) {
 		do {
 			Rx_Length = read(DriverFileDescriptor, response, NULL);
 		} while (Rx_Length == 0);
+		pthread_mutex_unlock(&rx_mutex);
 	}
+	
 
 	return 0;
 }

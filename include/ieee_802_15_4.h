97529461f752e3c0bc03d35566713b9f450c2de7
harry <h.morris@cascoda.com>
2017-05-03 13:51:28 +0100

    Add more documentation and enums
    Add more documentation and start changing grouped macros to enums in
    ieee802154.h.

--- include/ieee_802_15_4.h
+++ include/ieee_802_15_4.h
@@ -22,92 +22,6 @@
 #ifndef IEEE_802_15_4_H
 #define IEEE_802_15_4_H
 
-
-/******************************************************************************/
-/****** MAC Status Enumerations                                          ******/
-/******************************************************************************/
-#define MAC_SUCCESS                     (0x00)
-#define MAC_ERROR                       (0x01)
-#define MAC_CANCELLED                   (0x02)
-#define MAC_READY_FOR_POLL              (0x03)
-#define MAC_COUNTER_ERROR               (0xDB)
-#define MAC_IMPROPER_KEY_TYPE           (0xDC)
-#define MAC_IMPROPER_SECURITY_LEVEL     (0xDD)
-#define MAC_UNSUPPORTED_LEGACY          (0xDE)
-#define MAC_UNSUPPORTED_SECURITY        (0xDF)
-#define MAC_BEACON_LOST                 (0xE0)
-#define MAC_CHANNEL_ACCESS_FAILURE      (0xE1)
-#define MAC_DENIED                      (0xE2)
-#define MAC_DISABLE_TRX_FAILURE         (0xE3)
-#define MAC_SECURITY_ERROR              (0xE4)
-#define MAC_FRAME_TOO_LONG              (0xE5)
-#define MAC_INVALID_GTS                 (0xE6)
-#define MAC_INVALID_HANDLE              (0xE7)
-#define MAC_INVALID_PARAMETER           (0xE8)
-#define MAC_NO_ACK                      (0xE9)
-#define MAC_NO_BEACON                   (0xEA)
-#define MAC_NO_DATA                     (0xEB)
-#define MAC_NO_SHORT_ADDRESS            (0xEC)
-#define MAC_OUT_OF_CAP                  (0xED)
-#define MAC_PAN_ID_CONFLICT             (0xEE)
-#define MAC_REALIGNMENT                 (0xEF)
-#define MAC_TRANSACTION_EXPIRED         (0xF0)
-#define MAC_TRANSACTION_OVERFLOW        (0xF1)
-#define MAC_TX_ACTIVE                   (0xF2)
-#define MAC_UNAVAILABLE_KEY             (0xF3)
-#define MAC_UNSUPPORTED_ATTRIBUTE       (0xF4)
-#define MAC_INVALID_ADDRESS             (0xF5)
-#define MAC_ON_TIME_TOO_LONG            (0xF6)
-#define MAC_PAST_TIME                   (0xF7)
-#define MAC_TRACKING_OFF                (0xF8)
-#define MAC_INVALID_INDEX               (0xF9)
-#define MAC_LIMIT_REACHED               (0xFA)
-#define MAC_READ_ONLY                   (0xFB)
-#define MAC_SCAN_IN_PROGRESS            (0xFC)
-#define MAC_SUPERFRAME_OVERLAP          (0xFD)
-#define MAC_SYSTEM_ERROR                (0xFF)
-
-
-/******************************************************************************/
-/****** MAC Address Mode Definitions                                     ******/
-/******************************************************************************/
-#define MAC_MODE_NO_ADDR                ((unsigned)0x00)
-#define MAC_MODE_RESERVED               ((unsigned)0x01)
-#define MAC_MODE_SHORT_ADDR             ((unsigned)0x02)
-#define MAC_MODE_LONG_ADDR              ((unsigned)0x03)
-
-
-/******************************************************************************/
-/****** MAC Address Code Definitions                                     ******/
-/******************************************************************************/
-// Made from Combinations of Source and Destination Addressing Modes
-#define AC_INVALID                      (0xFF)
-/** No dst pan id, no dst address, src pan id, short src address */
-#define AC_DP0DA0SP2SA2                 (0)
-/** No dst pan id, no dst address, src pan id, extended src address */
-#define AC_DP0DA0SP2SA8                 (1)
-/** Dst pan id, short dst address, no src pan id, no src address */
-#define AC_DP2DA2SP0SA0                 (2)
-/** Dst pan id, extended dst address, no src pan id, no src address */
-#define AC_DP2DA8SP0SA0                 (3)
-/** Dst pan id, short dst address, no src pan id, short src address */
-#define AC_DP2DA2SP0SA2                 (4)
-/** Dst pan id, extended dst address, no src pan id, short src address */
-#define AC_DP2DA8SP0SA2                 (5)
-/** Dst pan id, short dst address, no src pan id, extended src address */
-#define AC_DP2DA2SP0SA8                 (6)
-/** Dst pan id, extended dst address, no src pan id, extended src address */
-#define AC_DP2DA8SP0SA8                 (7)
-/** Dst pan id, short dst address, src pan id, short src address */
-#define AC_DP2DA2SP2SA2                 (8)
-/** Dst pan id, extended dst address, src pan id, short src address */
-#define AC_DP2DA8SP2SA2                 (9)
-/** Dst pan id, short dst address, src pan id, extended src address */
-#define AC_DP2DA2SP2SA8                 (10)
-/** Dst pan id, extended dst address, src pan id, extended src address */
-#define AC_DP2DA8SP2SA8                 (11)
-
-
 /******************************************************************************/
 /****** MAC Frame Control Field Definitions                              ******/
 /******************************************************************************/
@@ -147,107 +61,12 @@
 /** Frame control source addressing mode getter */
 #define MAC_FC_SAM(fc)                  ((fc>>14)&3)
 
-
 /******************************************************************************/
 /****** MAC TxOption Bits Definitions                                    ******/
 /******************************************************************************/
-#define TXOPT_ACKREQ                    (0x01)
-#define TXOPT_GTS                       (0x02)
-#define TXOPT_INDIRECT                  (0x04)
-
-
-/******************************************************************************/
-/****** MAC ScanType Parameter for MLME_SCAN Request and Confirm         ******/
-/******************************************************************************/
-#define ENERGY_DETECT                   (0x00)
-#define ACTIVE_SCAN                     (0x01)
-#define PASSIVE_SCAN                    (0x02)
-#define ORPHAN_SCAN                     (0x03)
-
-
-/******************************************************************************/
-/****** MAC Command Frame Identifiers                                    ******/
-/******************************************************************************/
-#define CMD_ASSOCIATION_REQ         (1)
-#define CMD_ASSOCIATION_RSP         (2)
-#define CMD_DISASSOCIATION_NFY      (3)
-#define CMD_DATA_REQ                (4)
-#define CMD_PANID_CONFLICT_NFY      (5)
-#define CMD_ORPHAN_NFY              (6)
-#define CMD_BEACON_REQ              (7)
-#define CMD_COORD_REALIGN           (8)
-#define CMD_GTS_REQ                 (9)
-
-
-/******************************************************************************/
-/****** PHY/MAC PIB Attribute Enumerations                               ******/
-/******************************************************************************/
-#define phyCurrentChannel               (0x00)
-#define phyChannelsSupport              (0x01)
-#define phyTransmitPower                (0x02)
-#define phyCCAMode                      (0x03)
-#define phyCurrentPage                  (0x04)
-#define phyMaxFrameDuration             (0x05)
-#define phySHRDuration                  (0x06)
-#define phySymbolsPerOctet              (0x07)
-
-#define phyPibFirst                     (phyCurrentChannel)
-#define phyPibLast                      (phySymbolsPerOctet)
-
-#define macAckWaitDuration              (0x40)
-#define macAssociationPermit            (0x41)
-#define macAutoRequest                  (0x42)
-#define macBattLifeExt                  (0x43)
-#define macBattLifeExtPeriods           (0x44)
-#define macBeaconPayload                (0x45)
-#define macBeaconPayloadLength          (0x46)
-#define macBeaconOrder                  (0x47)
-#define macBeaconTxTime                 (0x48)
-#define macBSN                          (0x49)
-#define macCoordExtendedAddress         (0x4a)
-#define macCoordShortAddress            (0x4b)
-#define macDSN                          (0x4c)
-#define macGTSPermit                    (0x4d)
-#define macMaxCSMABackoffs              (0x4e)
-#define macMinBE                        (0x4f)
-#define macPANId                        (0x50)
-#define macPromiscuousMode              (0x51)
-#define macRxOnWhenIdle                 (0x52)
-#define macShortAddress                 (0x53)
-#define macSuperframeOrder              (0x54)
-#define macTransactionPersistenceTime	(0x55)
-#define macAssociatedPANCoord           (0x56)
-#define macMaxBE                        (0x57)
-#define macMaxFrameTotalWaitTime        (0x58)
-#define macMaxFrameRetries              (0x59)
-#define macResponseWaitTime             (0x5A)
-#define macSyncSymbolOffset             (0x5B)
-#define macTimestampSupported           (0x5C)
-#define macSecurityEnabled              (0x5D)
-
-#define macPibFirst                     (macAckWaitDuration)
-#define macPibLast                      (macSecurityEnabled)
-
-#define macKeyTable                     (0x71)
-#define macKeyTableEntries              (0x72)
-#define macDeviceTable                  (0x73)
-#define macDeviceTableEntries           (0x74)
-#define macSecurityLevelTable           (0x75)
-#define macSecurityLevelTableEntries    (0x76)
-#define macFrameCounter                 (0x77)
-#define macAutoRequestSecurityLevel     (0x78)
-#define macAutoRequestKeyIdMode         (0x79)
-#define macAutoRequestKeySource         (0x7A)
-#define macAutoRequestKeyIndex          (0x7B)
-#define macDefaultKeySource             (0x7C)
-#define macPANCoordExtendedAddress      (0x7D)
-#define macPANCoordShortAddress         (0x7E)
-
-#define macSecPibFirst                  (macKeyTable)
-#define macSecPibLast                   (macPANCoordShortAddress)
-
-#define nsIEEEAddress                   (0xFF)    /* Non-standard IEEE address */
-
+#define TXOPT_ACKREQ      (0x01) //!< Request acknowledgement from receiving node
+#define TXOPT_GTS         (0x02) //!< Use guaranteed time slot
+#define TXOPT_INDIRECT    (0x04) //!< Transmit indirectly
 
 /******************************************************************************/
 /****** PHY/MAC Constants                                                ******/
@@ -273,12 +92,191 @@
 
 #define MAX_FRAME_DURATION              (266)
 
-
 /******************************************************************************/
 /****** Length Definitions for different Commands                        ******/
 /******************************************************************************/
 // Data Request includes 1 phylen, 2 fc, 1 dsn, 12 address, 1 payload, 2 fcs
 #define BASELEN_DATA_REQUEST            (19)
+
+/** MAC Status Codes (see 802.15.4 2006 spec table 78) */
+enum mac_status {
+	MAC_SUCCESS                   = 0x00,
+	MAC_ERROR                     = 0x01,
+	MAC_CANCELLED                 = 0x02,
+	MAC_READY_FOR_POLL            = 0x03,
+	MAC_COUNTER_ERROR             = 0xDB,
+	MAC_IMPROPER_KEY_TYPE         = 0xDC,
+	MAC_IMPROPER_SECURITY_LEVEL   = 0xDD,
+	MAC_UNSUPPORTED_LEGACY        = 0xDE,
+	MAC_UNSUPPORTED_SECURITY      = 0xDF,
+	MAC_BEACON_LOST               = 0xE0,
+	MAC_CHANNEL_ACCESS_FAILURE    = 0xE1,
+	MAC_DENIED                    = 0xE2,
+	MAC_DISABLE_TRX_FAILURE       = 0xE3,
+	MAC_SECURITY_ERROR            = 0xE4,
+	MAC_FRAME_TOO_LONG            = 0xE5,
+	MAC_INVALID_GTS               = 0xE6,
+	MAC_INVALID_HANDLE            = 0xE7,
+	MAC_INVALID_PARAMETER         = 0xE8,
+	MAC_NO_ACK                    = 0xE9,
+	MAC_NO_BEACON                 = 0xEA,
+	MAC_NO_DATA                   = 0xEB,
+	MAC_NO_SHORT_ADDRESS          = 0xEC,
+	MAC_OUT_OF_CAP                = 0xED,
+	MAC_PAN_ID_CONFLICT           = 0xEE,
+	MAC_REALIGNMENT               = 0xEF,
+	MAC_TRANSACTION_EXPIRED       = 0xF0,
+	MAC_TRANSACTION_OVERFLOW      = 0xF1,
+	MAC_TX_ACTIVE                 = 0xF2,
+	MAC_UNAVAILABLE_KEY           = 0xF3,
+	MAC_UNSUPPORTED_ATTRIBUTE     = 0xF4,
+	MAC_INVALID_ADDRESS           = 0xF5,
+	MAC_ON_TIME_TOO_LONG          = 0xF6,
+	MAC_PAST_TIME                 = 0xF7,
+	MAC_TRACKING_OFF              = 0xF8,
+	MAC_INVALID_INDEX             = 0xF9,
+	MAC_LIMIT_REACHED             = 0xFA,
+	MAC_READ_ONLY                 = 0xFB,
+	MAC_SCAN_IN_PROGRESS          = 0xFC,
+	MAC_SUPERFRAME_OVERLAP        = 0xFD,
+	MAC_SYSTEM_ERROR              = 0xFF
+};
+
+/** MAC Address Mode Definitions */
+enum mac_addr_mode {
+	MAC_MODE_NO_ADDR       = 0x00, //!< no address
+	MAC_MODE_RESERVED      = 0x01, //!< reserved
+	MAC_MODE_SHORT_ADDR    = 0x02, //!< 16-bit short address
+	MAC_MODE_LONG_ADDR     = 0x03  //!< 64-bit extended address
+};
+
+/**
+ * MAC Address Code Definitions
+ *
+ * Made from Combinations of Source and Destination Addressing Modes
+ */
+enum mac_addr_code {
+	/** Invalid addressing parameter combination */
+	AC_INVALID          = 0xFF,
+	/** No dst pan id, no dst address, src pan id, short src address */
+	AC_DP0DA0SP2SA2     = 0,
+	/** No dst pan id, no dst address, src pan id, extended src address */
+	AC_DP0DA0SP2SA8     = 1,
+	/** Dst pan id, short dst address, no src pan id, no src address */
+	AC_DP2DA2SP0SA0     = 2,
+	/** Dst pan id, extended dst address, no src pan id, no src address */
+	AC_DP2DA8SP0SA0     = 3,
+	/** Dst pan id, short dst address, no src pan id, short src address */
+	AC_DP2DA2SP0SA2     = 4,
+	/** Dst pan id, extended dst address, no src pan id, short src address */
+	AC_DP2DA8SP0SA2     = 5,
+	/** Dst pan id, short dst address, no src pan id, extended src address */
+	AC_DP2DA2SP0SA8     = 6,
+	/** Dst pan id, extended dst address, no src pan id, extended src address */
+	AC_DP2DA8SP0SA8     = 7,
+	/** Dst pan id, short dst address, src pan id, short src address */
+	AC_DP2DA2SP2SA2     = 8,
+	/** Dst pan id, extended dst address, src pan id, short src address */
+	AC_DP2DA8SP2SA2     = 9,
+	/** Dst pan id, short dst address, src pan id, extended src address */
+	AC_DP2DA2SP2SA8     = 10,
+	/** Dst pan id, extended dst address, src pan id, extended src address */
+	AC_DP2DA8SP2SA8     = 11
+};
+
+/**
+ * MAC ScanType Parameter for MLME_SCAN Request and Confirm
+ *
+ * See 802.15.4 2006 spec sections 7.1.11.1 and 7.5.2.1
+ */
+enum mlme_scan_type {
+	ENERGY_DETECT    = 0x00,
+	ACTIVE_SCAN      = 0x01,
+	PASSIVE_SCAN     = 0x02,
+	ORPHAN_SCAN      = 0x03
+}
+
+/** MAC Command Frame Identifiers */
+enum mac_cmd_frame_id {
+	CMD_ASSOCIATION_REQ      = 1,
+	CMD_ASSOCIATION_RSP      = 2,
+	CMD_DISASSOCIATION_NFY   = 3,
+	CMD_DATA_REQ             = 4,
+	CMD_PANID_CONFLICT_NFY   = 5,
+	CMD_ORPHAN_NFY           = 6,
+	CMD_BEACON_REQ           = 7,
+	CMD_COORD_REALIGN        = 8,
+	CMD_GTS_REQ              = 9
+};
+
+/** PHY/MAC PIB Attribute Enumerations */
+enum pib_attribute {
+	phyCurrentChannel              = 0x00,
+	phyChannelsSupport             = 0x01,
+	phyTransmitPower               = 0x02,
+	phyCCAMode                     = 0x03,
+	phyCurrentPage                 = 0x04,
+	phyMaxFrameDuration            = 0x05,
+	phySHRDuration                 = 0x06,
+	phySymbolsPerOctet             = 0x07,
+
+	phyPibFirst                    = phyCurrentChannel,
+	phyPibLast                     = phySymbolsPerOctet,
+
+	macAckWaitDuration             = 0x40,
+	macAssociationPermit           = 0x41,
+	macAutoRequest                 = 0x42,
+	macBattLifeExt                 = 0x43,
+	macBattLifeExtPeriods          = 0x44,
+	macBeaconPayload               = 0x45,
+	macBeaconPayloadLength         = 0x46,
+	macBeaconOrder                 = 0x47,
+	macBeaconTxTime                = 0x48,
+	macBSN                         = 0x49,
+	macCoordExtendedAddress        = 0x4a,
+	macCoordShortAddress           = 0x4b,
+	macDSN                         = 0x4c,
+	macGTSPermit                   = 0x4d,
+	macMaxCSMABackoffs             = 0x4e,
+	macMinBE                       = 0x4f,
+	macPANId                       = 0x50,
+	macPromiscuousMode             = 0x51,
+	macRxOnWhenIdle                = 0x52,
+	macShortAddress                = 0x53,
+	macSuperframeOrder             = 0x54,
+	macTransactionPersistenceTime  = 0x55,
+	macAssociatedPANCoord          = 0x56,
+	macMaxBE                       = 0x57,
+	macMaxFrameTotalWaitTime       = 0x58,
+	macMaxFrameRetries             = 0x59,
+	macResponseWaitTime            = 0x5A,
+	macSyncSymbolOffset            = 0x5B,
+	macTimestampSupported          = 0x5C,
+	macSecurityEnabled             = 0x5D,
+
+	macPibFirst                    = macAckWaitDuration,
+	macPibLast                     = macSecurityEnabled,
+
+	macKeyTable                    = 0x71,
+	macKeyTableEntries             = 0x72,
+	macDeviceTable                 = 0x73,
+	macDeviceTableEntries          = 0x74,
+	macSecurityLevelTable          = 0x75,
+	macSecurityLevelTableEntries   = 0x76,
+	macFrameCounter                = 0x77,
+	macAutoRequestSecurityLevel    = 0x78,
+	macAutoRequestKeyIdMode        = 0x79,
+	macAutoRequestKeySource        = 0x7A,
+	macAutoRequestKeyIndex         = 0x7B,
+	macDefaultKeySource            = 0x7C,
+	macPANCoordExtendedAddress     = 0x7D,
+	macPANCoordShortAddress        = 0x7E,
+
+	macSecPibFirst                 = macKeyTable,
+	macSecPibLast                  = macPANCoordShortAddress,
+
+	nsIEEEAddress                  = 0xFF   /* Non-standard IEEE address */
+};
 
 
 #endif  // IEEE_802_15_4_H
